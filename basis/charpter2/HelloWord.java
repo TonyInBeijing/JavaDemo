f3ab9ee6281f5c80efecf5cccd8e91f7bcc9a340
TonyInBeijing <1300565735@qq.com>
2021-01-21 14:33:30 +0800

    添加注释

--- basis/charpter2/HelloWord.java
+++ basis/charpter2/HelloWord.java
@@ -3,3 +3,8 @@ public class HelloWord{
         System.out.println("HelloWorld");
     }
 }
+
+// 1.编写Java代码
+// 2.终端运行命令 javac HelloWord.java
+// 3.编译生成 HelloWorld.class
+// 4.终端运行命令 java HelloWorld
