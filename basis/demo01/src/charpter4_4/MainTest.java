ee2871902f1d21057f9700da57425e2ab54f4f57
TonyInBeijing <1300565735@qq.com>
2021-01-22 14:42:42 +0800

    java数据类型和转换

---
+++ basis/demo01/src/charpter4_4/MainTest.java
@@ -0,0 +1,25 @@
+package charpter4_4;
+
+public class MainTest {
+    static byte b1 = 1;
+    static short b2 = 100;
+    static int b3 = 32786;
+    static long b4 = 12123L;
+    static float b5 = 2.5F;
+    static double b6 = 2.6;
+    static boolean b7 = false;
+    static char b8 = 'a';
+    public static void main(String[] args) {
+        float f = 2.5F;
+        int i = (int)f;
+        System.out.println(i);
+//        System.out.println(b1);
+//        System.out.println(b2);
+//        System.out.println(b3);
+//        System.out.println(b4);
+//        System.out.println(b5);
+//        System.out.println(b6);
+//        System.out.println(b7);
+//        System.out.println(b8);
+    }
+}
