04b55e26273dd1d53682af07ca4864b183a17aab
TonyInBeijing <1300565735@qq.com>
2021-01-26 15:49:45 +0800

    ResultMap复杂对象一对一查询结果映射association

--- demo02-mybatis/src/main/java/net/xdclass/online_class/sqlSessionDemo.java
+++ demo02-mybatis/src/main/java/net/xdclass/online_class/sqlSessionDemo.java
@@ -1,7 +1,9 @@
 package net.xdclass.online_class;
 
 import net.xdclass.online_class.dao.VideoMapper;
+import net.xdclass.online_class.dao.VideoOrderMapper;
 import net.xdclass.online_class.domain.Video;
+import net.xdclass.online_class.domain.VideoOrder;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -21,44 +23,58 @@ public class sqlSessionDemo {
         // 获取session
         try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
             VideoMapper videoMapper = sqlSession.getMapper(VideoMapper.class);
-            Video video1 = new Video();
-            video1.setTitle("小滴课堂面试专题300道");
-            video1.setCoverImg("xdclass.net/aaa.png");
-            video1.setPoint(9.4);
-            video1.setCreateTime(new Date());
-            video1.setPrice(9900);
-            video1.setSummary("这个是面试专题概要");
+            VideoOrderMapper videoOrderMapper = sqlSession.getMapper(VideoOrderMapper.class);
+            /**
+             * 增
+             */
+//            Video video1 = new Video();
+//            video1.setTitle("小滴课堂面试专题300道");
+//            video1.setCoverImg("xdclass.net/aaa.png");
+//            video1.setPoint(9.4);
+//            video1.setCreateTime(new Date());
+//            video1.setPrice(9900);
+//            video1.setSummary("这个是面试专题概要");
+//            int row = videoMapper.add(video1);
+//            sqlSession.commit();
+//            System.out.println(row);
+            // 增加多条
+//            List<Video> list = new ArrayList<>();
+//            list.add(video1);
+//            list.add(video1);
+//            int rows1 = videoMapper.addBatch(list);
+//            sqlSession.commit();
+//            System.out.println(rows1);
+//            System.out.println(list.toString());
 
-            Video video2 = new Video();
-            video2.setTitle("小滴课堂面试专题400道");
-            video2.setCoverImg("xdclass.net/bbb.png");
-            video2.setPoint(8.0);
-            video2.setCreateTime(new Date());
-            video2.setPrice(2900);
-            video2.setSummary("这个是面试专题概要2");
+            /**
+             * 删
+             */
+//            Map<String, Object> map = new HashMap<>();
+//            map.put("createTime", "2021-01-11 09:33:20");
+//            map.put("price", 1000);
+//            int rows2 = videoMapper.deleteByCreateTimeAndPrice(map);
+//            sqlSession.commit();
+//            System.out.println(rows2);
 
+            /**
+             * 改
+             */
 //            Video video3 = new Video();
 //            video3.setTitle("测试视频1");
-////            video3.setSummary("这是测试视频3");
+//            video3.setSummary("这是测试视频3");
 //            video3.setId(66);
 //            videoMapper.updateVideoSelective(video3);
 //            sqlSession.commit();
 //            System.out.println(video3.toString());
-
-            Map<String, Object> map = new HashMap<>();
-            map.put("createTime", "2021-01-11 09:33:20");
-            map.put("price", 1000);
-            int rows = videoMapper.deleteByCreateTimeAndPrice(map);
-            sqlSession.commit();
-            System.out.println(rows);
-
-//            List<Video> list = new ArrayList<>();
-//            list.add(video1);
-//            list.add(video2);
-//            int rows = videoMapper.addBatch(list);
-//            sqlSession.commit();
-//            System.out.println(rows);
-//            System.out.println(list.toString());
+//
+            /**
+             * 查
+             */
+//            Video video2 = videoMapper.selectById(66);
+//            System.out.println(video2.toString());
+            // resultMap association关联查询
+            List<VideoOrder> videoOrders = videoOrderMapper.queryVideoOrderList();
+            System.out.println(videoOrders.toString());
         }
     }
 }
