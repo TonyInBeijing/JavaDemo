628ce822e43f206162f5727cf0a4eba67c722129
TonyInBeijing <1300565735@qq.com>
2021-01-27 15:33:53 +0800

    bean依赖注入

--- demo03-Spring/src/main/java/net/xdclass/sp/App.java
+++ demo03-Spring/src/main/java/net/xdclass/sp/App.java
@@ -1,13 +1,18 @@
 package net.xdclass.sp;
 
 import net.xdclass.sp.domain.Video;
+import net.xdclass.sp.domain.VideoOrder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Video video = (Video) applicationContext.getBean("video");
-        System.out.println(video.getTitle());
+//        Video video = (Video) applicationContext.getBean("video");
+//        System.out.println(video.getTitle());
+
+        VideoOrder videoOrder = (VideoOrder) applicationContext.getBean("videoOrder");
+        System.out.println(videoOrder.getOutTradeNo());
+        System.out.println(videoOrder.getVideo().getTitle());
     }
 }
---
+++ demo03-Spring/src/main/java/net/xdclass/sp/domain/VideoOrder.java
@@ -0,0 +1,31 @@
+package net.xdclass.sp.domain;
+
+public class VideoOrder {
+    private int id;
+    private Video video;
+    private String outTradeNo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+
+    public void setVideo(Video video) {
+        this.video = video;
+    }
+
+    public String getOutTradeNo() {
+        return outTradeNo;
+    }
+
+    public void setOutTradeNo(String outTradeNo) {
+        this.outTradeNo = outTradeNo;
+    }
+}
