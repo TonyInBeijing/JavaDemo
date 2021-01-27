e9a9655edb810cf6bc708f35b2d2295ee1096928
TonyInBeijing <1300565735@qq.com>
2021-01-27 15:56:33 +0800

    bean作用域

--- demo03-Spring/src/main/java/net/xdclass/sp/App.java
+++ demo03-Spring/src/main/java/net/xdclass/sp/App.java
@@ -8,11 +8,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-//        Video video = (Video) applicationContext.getBean("video");
-//        System.out.println(video.getTitle());
-
-        VideoOrder videoOrder = (VideoOrder) applicationContext.getBean("videoOrder");
-        System.out.println(videoOrder.getOutTradeNo());
-        System.out.println(videoOrder.getVideo().getTitle());
+        testScope(applicationContext);
+//        依赖注入
+//        VideoOrder videoOrder = (VideoOrder) applicationContext.getBean("videoOrder");
+//        System.out.println(videoOrder.getOutTradeNo());
+//        System.out.println(videoOrder.getVideo().getTitle());
+    }
+    private static void testScope(ApplicationContext context){
+        Video video1 = (Video)context.getBean("video");
+        Video video2 = (Video)context.getBean("video");
+        // 靠匹配内存地址 == 是匹配内存地址
+        System.out.println(video1 == video2);
     }
 }
