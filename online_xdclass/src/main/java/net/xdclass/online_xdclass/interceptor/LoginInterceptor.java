f7b3c6d3af99c2e5662760116ea4722a30c8409e
TonyInBeijing <1300565735@qq.com>
2021-02-01 15:31:43 +0800

    登录拦截器开发

---
+++ online_xdclass/src/main/java/net/xdclass/online_xdclass/interceptor/LoginInterceptor.java
@@ -0,0 +1,61 @@
+package net.xdclass.online_xdclass.interceptor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import net.xdclass.online_xdclass.utils.JWTUtils;
+import net.xdclass.online_xdclass.utils.JsonData;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * 登录拦截器开发
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        try {
+            String accessToken = request.getHeader("token");
+            if (accessToken == null) {
+                accessToken = request.getParameter("token");
+            }
+            if (StringUtils.isNoneBlank(accessToken)) {
+                Claims claims = JWTUtils.checkJWT(accessToken);
+                if (claims == null) {
+                    //登录过期，重新登录
+                    sendJsonMessage(response, JsonData.buildError("登录过期，重新登录"));
+                    return false;
+                }
+                Integer id = (Integer) claims.get("id");
+                String name = (String) claims.get("name");
+                request.setAttribute("user_id", id);
+                request.setAttribute("name", name);
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 响应Json数据给前端
+     *
+     * @param response
+     * @param object
+     */
+    public static void sendJsonMessage(HttpServletResponse response, Object object) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            response.setContentType("application/json; charset-utf-8");
+            PrintWriter printWriter = response.getWriter();
+            printWriter.print(objectMapper.writeValueAsString(object));
+            response.flushBuffer();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
